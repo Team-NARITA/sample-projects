986d169119f58d44943492f1b34aa309932897d3
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-06-13 09:21:12 +0900

    APIのコントローラークラスを追加

---
+++ back-end/src/main/kotlin/com/teamnarita/sample/sampleserver/api/UserController.kt
@@ -0,0 +1,23 @@
+package com.teamnarita.sample.sampleserver.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("v1/users")
+class UserController {
+
+    @PostMapping("/")
+    fun createUser() {
+
+    }
+
+    @GetMapping("/{userId}")
+    fun getUser(@PathVariable("userId") userId: String) {
+
+    }
+}
+
